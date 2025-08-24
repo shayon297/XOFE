4e044d7215f689e143e7918ced88e5d655a66ec8
Shayon Sengupta <shayon297@gmail.com>
2025-08-24 18:41:03 -0400

    add bundle debugging to track import issues

--- src/turnkey-bundle.js
+++ src/turnkey-bundle.js
@@ -1,6 +1,11 @@
 // Bundle Turnkey SDK for Chrome extension use
 import { TurnkeyBrowserClient, TurnkeyPasskeyClient, WebauthnStamper } from '@turnkey/sdk-browser';
 
+console.log('XOFE Bundle: Starting to set up Turnkey SDK...');
+console.log('XOFE Bundle: TurnkeyBrowserClient imported:', !!TurnkeyBrowserClient);
+console.log('XOFE Bundle: TurnkeyPasskeyClient imported:', !!TurnkeyPasskeyClient);
+console.log('XOFE Bundle: WebauthnStamper imported:', !!WebauthnStamper);
+
 // Export Turnkey to global window object for use in extension
 window.TurnkeySDK = {
   TurnkeyBrowserClient: TurnkeyBrowserClient,
@@ -8,4 +13,5 @@ window.TurnkeySDK = {
   WebauthnStamper: WebauthnStamper
 };
 
+console.log('XOFE Bundle: window.TurnkeySDK set to:', window.TurnkeySDK);
 console.log('Turnkey SDK with Solana support bundled and ready for Chrome extension');
